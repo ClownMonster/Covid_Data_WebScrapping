a6d4b8ab8d3b1b5ea274f3c4d8288f51991a8d53
ClownMonster <mk6386223@gmail.com>
2020-04-13 20:36:12 +0530

    visualization bar

--- Visual.py
+++ Visual.py
@@ -1,8 +1,19 @@
 import pandas as pd
 import plotly.express as px
+from plotly import graph_objects as go
 
 df = pd.read_excel('./CovidData.xlsx')
 
-fig = px.bar(df.head(100), x = 'Country Name', y = 'Total Cases', color = 'Total Cases' )
-fig.update_layout(title_text = 'Clown Monsters Visualization')
+#fig = px.bar(df.head(100), x = 'Country Name', y = 'Total Cases', color = 'Total Cases' )
+#fig.update_layout(title_text = 'Clown Monsters Visualization')
+#fig.show()
+
+
+fig = go.Figure()
+#df.sort_values(df['Total Deaths per 1M'], ascending = False, inplace = True)
+df = df.head(150)
+
+fig.add_trace( go.Bar(y = df['Total Deaths Cases'], x = df['Country Name'], marker = {'colorscale': 'Viridis', 'color': 'rebeccapurple' }))
+fig.update_layout(title_text = 'Heat Map Visulation', barmode = 'stack')
+
 fig.show()
